912c59061d5b3fb477c5d37743e96e372c3b1d1f
Aisosa Matthew <aisosamatthew247@gmail.com>
2025-08-14 02:47:57 +0100

    Created the menu toggle

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 const image = document.querySelector('#intro-image');
+const openBtn = document.querySelector('#hamburger');
+const closeBtn = document.querySelector('#close');
+const header = document.querySelector('header');
 
 const imageChange = () => {
     if(window.innerWidth <= 1104 ) {
@@ -8,5 +11,20 @@ const imageChange = () => {
     }
 }
 
-window.onresize = imageChange();
+const openMenu = (event)  => {
+    const menuList = event.target.parentElement.nextElementSibling;
+    menuList.style.display = 'block';
+    header.style.backgroundColor =  'hsl(233, 8%, 62%)';
+}
+
+const closeMenu = (event)  => {
+    const menuList = event.target.parentElement;
+    menuList.style.display = 'none';
+    header.style.backgroundColor =  'hsl(0, 100%, 100%)';
+}
+
+openBtn.addEventListener('click', openMenu);
+closeBtn.addEventListener('click', closeMenu);
+
+window.addEventListener('resize', imageChange);
 window.onload = imageChange();
